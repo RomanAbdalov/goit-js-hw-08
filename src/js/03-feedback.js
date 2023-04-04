19ae35c36155952519f4b51872fb90383956f13f
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-04-04 16:36:57 +0300

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector('feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+    const formData ={
+          }
+}
