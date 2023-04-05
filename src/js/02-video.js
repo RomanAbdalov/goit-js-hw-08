7834bf1fdfc4a6b7ed4a52aee19c9538267d96cf
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-04-05 17:27:22 +0300

    add task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,8 +8,7 @@ const player = new Player(iframe);
 const onPlay = function(data) {
     localStorage.setItem("videoplayer-current-time", data.seconds);
     };
-    
-
-    player.on('timeupdate', throttle(onPlay, 1000));
-    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+   
+ player.on('timeupdate', throttle(onPlay, 1000));
+ player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
     
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,30 @@
-// import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-// const STORAGE_KEY = "feedback-form-state";
-// const form = document.querySelector('feedback-form');
+const LOCAL_KEY = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
 
-// form.addEventListener('input', throttle(onFormInput, 500));
-// form.addEventListener('submit', onFormSubmit);
+const form = document.querySelector('.feedback-form')
+form.addEventListener('input', throttle(storageFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+reloadPage();
 
-// function onFormInput(event) {
-//     const formData ={
-//           }
-// }
+function storageFormData(element) {
+    formData[element.target.name] = element.target.value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(element) {
+    element.preventDefault();
+    console.log(formData);
+    element.currentTarget.reset();
+    localStorage.removeItem(LOCAL_KEY);
+    formData = {};
+};
+
+function reloadPage() {
+    if (formData) {
+        let { email, message } = form.elements;
+        email.value = formData.email || '';
+        message.value = formData.message || '';
+      };
+  };
